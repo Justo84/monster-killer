709e26d9d8a59f10a8e32e0edfd9ed715deecaea
Justin Egan <justin.d.egan@gmail.com>
2023-01-04 11:47:24 -0500

    strong attack

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,16 +1,41 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
+
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife
 let currentPlayerHealth = chosenMaxLife
 
 adjustHealthBars(chosenMaxLife)
 
-const attackHandler = () => {
-    const damage = dealMonsterDamage(ATTACK_VALUE)
-    currentMonsterHealth -= damage
-    if (currentMonsterHealth <= 0) {
-        alert("you won")
+const attackMonster = (attackMode) => {
+    let maxDamage;
+    if (attackMode === "ATTACK") {
+        maxDamage = ATTACK_VALUE
+    } else if ("STRONG_ATTACK") {
+        maxDamage = STRONG_ATTACK_VALUE
     }
+    const damage = dealMonsterDamage(maxDamage)
+    currentMonsterHealth -= damage
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
+    currentPlayerHealth -= playerDamage
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert("you won")
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert("you lost")
+    } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+        alert("Draw!")
+    }
+
+}
+
+const attackHandler = () => {
+    attackMonster("ATTACK")
+}
+
+const strongAttackHandler = () => {
+    attackMonster("STRONG_ATTACK")
 }
 
 attackBtn.addEventListener('click', attackHandler)
+strongAttackBtn.addEventListener('click', strongAttackHandler)
