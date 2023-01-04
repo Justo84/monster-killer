ff105b9598a2ab1fd9eea0b9168ca4a9f597d7cf
Justin Egan <justin.d.egan@gmail.com>
2023-01-04 12:42:03 -0500

    endRound condition

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,12 +1,25 @@
 const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife
 let currentPlayerHealth = chosenMaxLife
 
 adjustHealthBars(chosenMaxLife)
+
+function endRound() {
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
+    currentPlayerHealth -= playerDamage
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert("you won")
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert("you lost")
+    } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+        alert("Draw!")
+    }
+}
 
 const attackMonster = (attackMode) => {
     let maxDamage;
@@ -17,16 +30,7 @@ const attackMonster = (attackMode) => {
     }
     const damage = dealMonsterDamage(maxDamage)
     currentMonsterHealth -= damage
-    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
-    currentPlayerHealth -= playerDamage
-    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-        alert("you won")
-    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-        alert("you lost")
-    } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
-        alert("Draw!")
-    }
-
+    endRound()
 }
 
 const attackHandler = () => {
@@ -37,5 +41,11 @@ const strongAttackHandler = () => {
     attackMonster("STRONG_ATTACK")
 }
 
+const healPlayerHandler = () => {
+    increasePlayerHealth(HEAL_VALUE)
+    endRound()
+}
+
 attackBtn.addEventListener('click', attackHandler)
 strongAttackBtn.addEventListener('click', strongAttackHandler)
+healBtn.addEventListener('click', healPlayerHandler)
